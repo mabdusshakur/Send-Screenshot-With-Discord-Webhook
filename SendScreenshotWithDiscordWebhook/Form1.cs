d898f0d43caef432897fac7287174d4d4fde4aae
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-20 14:54:05 +0600

    first commit

---
+++ SendScreenshotWithDiscordWebhook/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using System.Windows.Forms;
+
+
+namespace SendScreenshotWithDiscordWebhook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sendScreenshot_btn_Click(object sender, EventArgs e)
+        {
+            var filePath = Environment.CurrentDirectory + "screenshot.png";
+
+            var s_width = Screen.PrimaryScreen.Bounds.Width;
+            var s_height = Screen.PrimaryScreen.Bounds.Height;
+
+            Bitmap bitmap = new Bitmap(s_width, s_height);
+            Rectangle rectangle = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(rectangle.Left, rectangle.Top, 0, 0, rectangle.Size);
+            bitmap.Save(filePath, ImageFormat.Png);
+
+            HttpClient client = new HttpClient();
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+
+            var file = File.ReadAllBytes(filePath);
+            multipartFormDataContent.Add(new ByteArrayContent(file, 0, file.Length), Path.GetExtension(filePath), filePath);
+            client.PostAsync("https://discord.com/api/webhooks/1176074138964742174/mG7sJlHUz9R2rI7LqLgIXkvCUFb0GNZWpro6vdADfzGJW7ELgW3JVWxTdszna-aIrGF2", multipartFormDataContent).Wait();
+            client.Dispose();
+        }
+    }
+}
